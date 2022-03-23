20ad0a5f7fbb71a5769feb8ff57c61a1679bbcf7
Allen <1942813644@qq.com>
2022-03-23 13:09:52 +0800

    安装了 React 和 ReactDOM

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
 
-const div = document.createElement('div')
-div.innerText='hi'
-document.body.appendChild(div)
+console.log(React);
+console.log(ReactDOM);
